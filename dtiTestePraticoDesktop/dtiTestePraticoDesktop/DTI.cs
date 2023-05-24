5bf151fbf1773dc93453bf04fe2c4cc2c7f429ca
Gustavo Rafá <48700475+GustavoRafa@users.noreply.github.com>
2023-05-24 02:47:44 -0300

    Add files via upload

---
+++ dtiTestePraticoDesktop/dtiTestePraticoDesktop/DTI.cs
@@ -0,0 +1,135 @@
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace dtiTestePraticoDesktop
+{
+    public partial class DTI : Form
+    {
+        private List<Lembrete> listaLembretes = new List<Lembrete>();
+        public DTI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+            // Validação dos campos
+            if (string.IsNullOrEmpty(txtNome.Text))
+            {
+                MessageBox.Show("O campo 'Nome' deve estar preenchido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (dateTimePicker.Value <= DateTime.Now)
+            {
+                MessageBox.Show("A data do lembrete deve estar no futuro.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Criação do lembrete
+            Lembrete lembrete = new Lembrete(txtNome.Text, dateTimePicker.Value);
+
+            // Verifica se o dia já existe na lista de lembretes
+            var diaExistente = listaLembretes.FirstOrDefault(d => d.Data.Date == lembrete.Data.Date);
+            if (diaExistente != null)
+            {
+                // Adiciona o lembrete ao dia existente
+                diaExistente.Lembretes.Add(lembrete);
+            }
+            else
+            {
+                // Cria um novo dia com o lembrete
+                listaLembretes.Add(new Dia(lembrete.Data, lembrete));
+            }
+
+            // Ordena a lista de lembretes por data
+            listaLembretes = listaLembretes.OrderBy(d => d.Data).ToList();
+
+            // Limpa os campos
+            txtNome.Text = "";
+            dateTimePicker.Value = DateTime.Today;
+
+            // Atualiza a lista de lembretes
+            AtualizarListaLembretes();
+        }
+
+        private void AtualizarListaLembretes()
+        {
+            listViewLembretes.Items.Clear();
+
+            foreach (var dia in listaLembretes)
+            {
+                foreach (var lembrete in dia.Lembretes)
+                {
+                    var item = new ListViewItem(new[] { dia.Data.ToShortDateString(), lembrete.Nome });
+                    listViewLembretes.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            if (listViewLembretes.SelectedItems.Count > 0)
+            {
+                var itemSelecionado = listViewLembretes.SelectedItems[0];
+                var dataLembrete = DateTime.Parse(itemSelecionado.SubItems[0].Text);
+                var nomeLembrete = itemSelecionado.SubItems[1].Text;
+
+                // Procura o dia correspondente ao lembrete
+                var dia = listaLembretes.FirstOrDefault(d => d.Data.Date == dataLembrete.Date);
+
+                if (dia != null)
+                {
+                    // Remove o lembrete da lista
+                    var lembrete = dia.Lembretes.FirstOrDefault(l => l.Nome == nomeLembrete);
+                    dia.Lembretes.Remove(lembrete);
+
+                    // Remove o dia se não houver mais lembretes
+                    if (dia.Lembretes.Count == 0)
+                    {
+                        listaLembretes.Remove(dia);
+                    }
+
+                    // Atualiza a lista de lembretes
+                    AtualizarListaLembretes();
+                }
+            }
+        }
+
+        private void DTI_Load(object sender, EventArgs e)
+        {
+            // Configurações iniciais
+            dateTimePicker.MinDate = DateTime.Today;
+        }
+
+
+        public class Lembrete
+        {
+            public string Nome { get; set; }
+            public DateTime Data { get; set; }
+
+            public Lembrete(string nome, DateTime data)
+            {
+                Nome = nome;
+                Data = data;
+            }
+        }
+
+        public class Dia
+        {
+            public DateTime Data { get; set; }
+            public List<Lembrete> Lembretes { get; set; }
+
+            public Dia(DateTime data, Lembrete lembrete)
+            {
+                Data = data;
+                Lembretes = new List<Lembrete> { lembrete };
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ dtiTestePraticoDesktop/dtiTestePraticoDesktop/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace dtiTestePraticoDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.listViewLembretes = new System.Windows.Forms.ListView();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(194, 82);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(239, 23);
+            this.txtNome.TabIndex = 0;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(194, 130);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(239, 23);
+            this.dateTimePicker.TabIndex = 1;
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Location = new System.Drawing.Point(358, 193);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(75, 23);
+            this.btnCriar.TabIndex = 2;
+            this.btnCriar.Text = "button1";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.btnCriar_Click);
+            // 
+            // listViewLembretes
+            // 
+            this.listViewLembretes.Location = new System.Drawing.Point(37, 222);
+            this.listViewLembretes.Name = "listViewLembretes";
+            this.listViewLembretes.Size = new System.Drawing.Size(709, 296);
+            this.listViewLembretes.TabIndex = 3;
+            this.listViewLembretes.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(489, 193);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.TabIndex = 4;
+            this.btnDeletar.Text = "button1";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 530);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.listViewLembretes);
+            this.Controls.Add(this.btnCriar);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNome;
+        private DateTimePicker dateTimePicker;
+        private Button btnCriar;
+        private ListView listViewLembretes;
+        private Button btnDeletar;
+    }
+}
